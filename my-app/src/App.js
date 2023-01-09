77caee54bf9b713f2fdf8ef9f016122298516093
Trevor Rapp <trrapp@churchofjesuschrist.org>
2023-01-08 19:01:47 -0700

    adds Header, Footer, Main components

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,9 +1,13 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import "./App.css";
 
 function App() {
   return (
-
+    <div className="">
+      <Header className="" />
+      <Main className="" />
+      <Foter className="" />
+    </div>
   );
 }
 
---
+++ my-app/src/components/Footer.js
@@ -0,0 +1,9 @@
+import React from "react";
+
+export default function Footer() {
+  return (
+    <div className="">
+      <h2>Footer</h2>
+    </div>
+  );
+}
