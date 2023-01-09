ef0438d91ef125e870c50987fce057ca8b8cca4c
Trevor Rapp <trrapp@churchofjesuschrist.org>
2023-01-08 20:14:46 -0700

    adds successful api request and adds color scheme to CSS

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,14 +1,25 @@
 import React from "react";
 import "./App.css";
+import Header from "./components/Header";
+import Main from "./components/Main";
+import Footer from "./components/Footer";
 
-function App() {
+export default function App() {
+  const [nasaData, setNasaData] = React.useState({});
+
+  fetch(
+    "https://api.nasa.gov/planetary/apod?api_key=gQBHOwp1QgftmdeZ3bO3KTnskprBBUwud1vmWgRz"
+  )
+    .then((res) => res.json())
+    .then((data) => {
+      console.log(data);
+    });
+
   return (
     <div className="">
       <Header className="" />
       <Main className="" />
-      <Foter className="" />
+      <Footer className="" />
     </div>
   );
 }
-
-export default App;
