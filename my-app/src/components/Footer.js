ef0def308e451462e02caa2ee2bc46e4446d33f0
Trevor Rapp <trrapp@churchofjesuschrist.org>
2023-01-17 22:43:56 -0700

    adds stuff, fixes stuff, CSS's stuff

--- my-app/src/components/Footer.js
+++ my-app/src/components/Footer.js
@@ -4,7 +4,7 @@ export default function Footer() {
   return (
     <div className="footer--container">
       <h3 className="footer--text">Information displayed is made available through <span className="blue"><a href="https://api.nasa.gov/">Nasa &#123; APIS &#125; </a></span></h3>
-      <p className="lower-opacity">All materials used as part of public domain</p>
+      <p className="lower-opacity">All materials used are part of the public domain</p>
     </div>
   );
 }
