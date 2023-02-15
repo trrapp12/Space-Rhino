f9fc17434ad3d8aa29066a19b3b6914b89a6688f
Trevor Rapp <trrapp@gmail.com>
2023-02-14 23:01:55 -0700

    makes the footer disappear on a scroll event so that it no longer hides the cards

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,10 +1,28 @@
 import React from "react";
 
 export default function Footer() {
+
+  const [visibility, toggleVisibility] = React.useState(true)
+
+  window.addEventListener('scroll', () => {
+    console.log('scrolling in footer')
+    const footer = document.getElementById('footer--container')
+    // if (visibility) {
+    //   console.log('visible')
+    //   toggleVisibility(!visibility)
+    // } else {
+    //   setTimeout(() => {
+    //     toggleVisibility(!visibility)
+    //     console.log('not visible')
+    //   }, 500)
+    // }
+    toggleVisibility(false)
+  })
+
   return (
-    <div className="footer--container">
-      <h3 className="footer--text">Information displayed is made available through <span className="blue"><a href="https://api.nasa.gov/">Nasa &#123; APIS &#125; </a></span></h3>
-      <p className="lower-opacity">All materials used are part of the public domain</p>
-    </div>
+      <div id="footer--container" className={visibility ? "footer--container visible" : "footer--container"}> 
+        <h3 className="footer--text">Information displayed is made available through <span className="blue"><a href="https://api.nasa.gov/">Nasa &#123; APIS &#125; </a></span></h3>
+        <p className="lower-opacity">All materials used are part of the public domain</p>
+      </div>
   );
 }
