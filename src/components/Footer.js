4e7ad5445924a53cb15e1c2931c708b48c30d04e
Trevor Rapp <trrapp@gmail.com>
2023-02-15 23:49:03 -0700

    fixes issue with sidebar always being in the way

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -11,9 +11,13 @@ export default function Footer() {
   })
 
   return (
+    <div className="footer--wrapper">
       <div id="footer--container" className={visibility ? "footer--container visible" : "footer--container"}> 
         <h3 className="footer--text">Information displayed is made available through <span className="blue"><a href="https://api.nasa.gov/">Nasa &#123; APIS &#125; </a></span></h3>
         <p className="lower-opacity">All materials used are part of the public domain</p>
       </div>
+    </div>
+
+    
   );
 }
